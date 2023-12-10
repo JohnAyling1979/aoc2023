8b2127e3337ee6cb3e0b14dd9e674556815bc380
John Ayling <john.ayling@digintent.com>
2023-12-10 13:55:44 -0600

    WIP

--- day10/part2.js
+++ day10/part2.js
@@ -5,13 +5,38 @@ const fs = require('fs');
 let start;
 const visited = {};
 
-const map = fs.readFileSync('./test3', 'utf8').split('\n').map((line, yIndex) => line.split('').map((char, xIndex) => {
+const map = fs.readFileSync('./data', 'utf8').split('\n').map((line, yIndex) => line.split('').map((char, xIndex) => {
   if (char === 'S') {
-    start = { x: xIndex, y: yIndex, direction: 'U' };
+    start = { x: xIndex, y: yIndex, direction: '^' };
   }
 
   return char;
 }));
+
+function floodFill(x, y) {
+  const directions = ['^', '>', 'v', '<'];
+
+  if (x < 0 || y < 0 || x >= map[0].length || y >= map.length) {
+    return;
+  }
+
+  const location = map[y][x];
+
+  if (directions.includes(location) || location === 'O') {
+    return;
+  }
+
+  map[y][x] = 'O';
+  const up = y - 1;
+  const down = y + 1;
+  const left = x - 1;
+  const right = x + 1;
+
+  floodFill(x, up);
+  floodFill(x, down);
+  floodFill(left, y);
+  floodFill(right, y);
+}
 
 function loadPaths(node) {
   const { x, y, direction } = node;
@@ -29,8 +54,8 @@ function loadPaths(node) {
   if (location === 'S') {
     const up = y - 1;
 
-    next.push({ x, y: up, direction: 'U' });
-    map[y][x] = 'U';
+    next.push({ x, y: up, direction: '^' });
+    map[y][x] = '^';
   } else if (location === '|') {
     const up = y - 1;
     const down = y + 1;
@@ -46,56 +71,56 @@ function loadPaths(node) {
     const left = x - 1;
     const right = x + 1;
 
-    map[y][x] = direction;
-
     if (!visited[`${left},${y}`]) {
-      next.push({ x: left, y, direction });
+      next.push({ x: left, y, direction: '<' });
+      map[y][x] = '<';
     } else {
-      next.push({ x: right, y, direction });
+      next.push({ x: right, y, direction: '>' });
+      map[y][x] = '>';
     }
   } else if (location === 'L') {
     const up = y - 1;
     const right = x + 1;
 
     if (!visited[`${x},${up}`]) {
-      next.push({ x, y: up, direction: 'U' });
-      map[y][x] = 'U';
+      next.push({ x, y: up, direction: '^' });
+      map[y][x] = '^';
     } else {
-      next.push({ x: right, y, direction: 'D' });
-      map[y][x] = 'D';
+      next.push({ x: right, y, direction: '>' });
+      map[y][x] = '>';
     }
   } else if (location === 'J') {
     const up = y - 1;
     const left = x - 1;
 
     if (!visited[`${x},${up}`]) {
-      next.push({ x, y: up, direction: 'U' });
-      map[y][x] = 'U';
+      next.push({ x, y: up, direction: '^' });
+      map[y][x] = '^';
     } else {
-      next.push({ x: left, y, direction: 'D' });
-      map[y][x] = 'D';
+      next.push({ x: left, y, direction: '<' });
+      map[y][x] = '<';
     }
   } else if (location === '7') {
     const down = y + 1;
     const left = x - 1;
 
     if (!visited[`${x},${down}`]) {
-      next.push({ x, y: down, direction: 'D' });
-      map[y][x] = 'D';
+      next.push({ x, y: down, direction: 'v' });
+      map[y][x] = 'v';
     } else {
-      next.push({ x: left, y, direction: 'U' });
-      map[y][x] = 'U';
+      next.push({ x: left, y, direction: '<' });
+      map[y][x] = '<';
     }
   } else if (location === 'F') {
     const down = y + 1;
     const right = x + 1;
 
     if (!visited[`${x},${down}`]) {
-      next.push({ x, y: down, direction: 'D' });
-      map[y][x] = 'D';
+      next.push({ x, y: down, direction: 'v' });
+      map[y][x] = 'v';
     } else {
-      next.push({ x: right, y, direction: 'U'});
-      map[y][x] = 'U';
+      next.push({ x: right, y, direction: '>'});
+      map[y][x] = '>';
     }
   }
 
@@ -105,41 +130,45 @@ function loadPaths(node) {
 
 loadPaths(start);
 
+for (let y = 0; y < map.length; y++) {
+  for (let x = 0; x < map[y].length; x++) {
+    if (y === 0 || y === map.length - 1 || x === 0 || x === map[y].length - 1) {
+      floodFill(x, y);
+    }
+  }
+}
+
 let totalCount = 0;
 
 for (let y = 0; y < map.length; y++) {
-  let counting = false;
-  let direction = null;
-  let possible = [];
+  const directionStack = [];
+  let possible = []
   for (let x = 0; x < map[y].length; x++) {
     char = map[y][x];
-    if (counting && !(char === 'U' || char === 'D')) {
-      possible.push(`${x},${y}`);
-    }
 
-    if (direction === null && (char === 'U' || char === 'D')) {
-      direction = char;
-      counting = true;
-    } else if (direction === 'U' && char === 'D') {
-      direction = null;
-      totalCount += possible.length;
-      counting = false;
+    if (char === '^' || char === 'v') {
+      if (directionStack.length === 0) {
+        directionStack.push(char);
+        continue;
+      } else {
+        const lastDirection = directionStack[directionStack.length - 1];
 
-      possible.forEach(key => {
-        const [x, y] = key.split(',');
-        map[y][x] = 'X';
-      });
+        if (lastDirection !== char) {
+          totalCount += possible.length;
 
-      possible = [];
-    } else if (direction === 'D' && char === 'U') {
-      direction = null;
-      totalCount += possible.length;
-      counting = false;
-      possible.forEach(key => {
-        const [x, y] = key.split(',');
-        map[y][x] = 'X';
-      });
-      possible = [];
+          possible.forEach(({ x, y }) => {
+            map[y][x] = 'I';
+          });
+          possible = [];
+          directionStack.pop();
+        }
+      }
+    } else if (char === '<' || char === '>' || char === 'O') {
+      // do nothing
+    } else {
+      if (directionStack.length > 0) {
+        possible.push({ x, y });
+      }
     }
   }
 }
